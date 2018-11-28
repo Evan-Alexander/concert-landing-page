93fc4e2e727785f3bd5754ec14847de909d89da3
jb <jasontbrown99@gmail.com>
2018-11-28 11:00:07 -0800

    Updates

---
+++ precache-manifest.018817006c2025eb8ec6cab3b942b9b0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "c966a59260377f3066942a1e7b61a76e",
+    "url": "/concert-landing-page/static/media/jose-playing.c966a592.jpg"
+  },
+  {
+    "revision": "039676527db32656a0e38559bfe5f32a",
+    "url": "/concert-landing-page/static/media/jose-gonzalez-playing2.03967652.jpg"
+  },
+  {
+    "revision": "cd026f19e0ecee7ca51381fe62c3ea27",
+    "url": "/concert-landing-page/static/media/jose-gonzalez-greenBg.cd026f19.jpg"
+  },
+  {
+    "revision": "3c9aa87c266381d741ce",
+    "url": "/concert-landing-page/static/js/runtime~main.3c9aa87c.js"
+  },
+  {
+    "revision": "caa4e217f202ba64b30d",
+    "url": "/concert-landing-page/static/js/main.caa4e217.chunk.js"
+  },
+  {
+    "revision": "a537087a56b2c050f6c1",
+    "url": "/concert-landing-page/static/js/1.a537087a.chunk.js"
+  },
+  {
+    "revision": "caa4e217f202ba64b30d",
+    "url": "/concert-landing-page/static/css/main.c331428d.chunk.css"
+  },
+  {
+    "revision": "579df9b37a70a9d9d8515bbb21f1148b",
+    "url": "/concert-landing-page/index.html"
+  }
+];
